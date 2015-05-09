8410f124b2d0fcf9a4105ccab5ab9c7b8b246e7a
Richard Machielse <richardmachielse@gmail.com>
2015-05-10 00:52:25 +0200

    Auto-update manifest version with package + never fingerprint when building production app

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-var path = require('path');
-var commands = require('./lib/commands');
-
 module.exports = {
   name: 'ember-cli-mobile-chrome-apps',
 
+  included: function(app) {
+    if (process.argv[2] === 'chrome:build') {
+      app.options.fingerprint.enabled = false;
+    }
+  },
+
   includedCommands: function() {
+    var commands = require('./lib/commands');
+
     return commands;
   },
 
   blueprintsPath: function() {
+    var path = require('path');
+
     return path.join(__dirname, 'blueprints');
   }
 }
